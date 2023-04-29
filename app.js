d3a3a95bc460814d943e70a009a289d812b722eb
dhirajtek13 <dhirajtek13@gmail.com>
2023-04-29 22:17:25 +0530

    3

--- app.js
+++ app.js
@@ -4,43 +4,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set("view engine", "ejs");
+
+var items = ["Buy Food", "cook Food", "Eat Food"];
+app.set("view engine", "ejs"); //for ejs template engine
+
+app.use(bodyParser.urlencoded({extended:true}));//for form body parser
 
 app.get("/", function (req, res) {
   // res.send("hello");
   const today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-    default:
-        console.log("Erro: current day is equal to "+currentDay);
-      break;
-  }
-  res.render("list", { kindOfDay: day });
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newItems: items });
 });
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("server started on port 3000.");
