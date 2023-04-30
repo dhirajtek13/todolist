3e31d2c4466f3e65c71de0a2d3e9be75250385bf
dhirajtek13 <dhirajtek13@gmail.com>
2023-04-30 07:30:27 +0530

    5

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 var items = ["Buy Food", "cook Food", "Eat Food"];
+var workItems = [];
 app.set("view engine", "ejs"); //for ejs template engine
 
 app.use(bodyParser.urlencoded({extended:true}));//for form body parser
@@ -22,14 +23,38 @@ app.get("/", function (req, res) {
   };
 
   let day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
 });
 
 app.post("/", function(req, res) {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
-})
+    if(req.body.list === "work") {
+        workItems.push(item);
+        res.redirect("/work");
+
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+
+
+app.get("/work", function (req, res) {
+    res.render("list", {listTitle:"work List", newItems: workItems});
+});
+
+// app.post("/work", function(req, res) {
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
+
+app.get('/about', function(req, res) {
+    res.render("about");
+});
+
+
 
 app.listen(3000, function () {
   console.log("server started on port 3000.");
